49793ff4d4bfd50c2d6552c95cd035a4ebdb9a49
Devonty <Devonty@yandex.ru>
2023-09-03 15:01:47 +0300

    Пустое окно

---
+++ src/ru/vsu/cs/kg2023/elfimov_a_m/task1_primitives/Main.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.kg2023.elfimov_a_m.task1_primitives;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mn = new MainWindow();
+        mn.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        mn.setSize(800, 600);
+        mn.setVisible(true);
+    }
+}
---
+++ src/ru/vsu/cs/kg2023/elfimov_a_m/task1_primitives/MainWindow.java
@@ -0,0 +1,7 @@
+package ru.vsu.cs.kg2023.elfimov_a_m.task1_primitives;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+}
